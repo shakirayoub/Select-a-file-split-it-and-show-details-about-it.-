bd01b8b74b7c43d596c8eb79cef01036e8ea6274
bhatnaushad <32789454+bhatnaushad@users.noreply.github.com>
2018-03-18 01:49:49 +0530

    A minor change in path

--- MainFile.java
+++ MainFile.java
@@ -97,7 +97,7 @@ public class Main {
         String OS = System.getProperty("os.name").toLowerCase();
         if(OS.indexOf("win") >= 0){
 
-            return "C:/Users/NAUSHAD/Desktop/";
+            return "C:/";
         } else if(OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 ){
             return "/home/";
         } else if(OS.indexOf("mac") >= 0){
