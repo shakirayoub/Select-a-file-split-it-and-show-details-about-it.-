18c461cf08bfad6b7b2f1b85ab5f3f9ed2325eac
bhatnaushad <32789454+bhatnaushad@users.noreply.github.com>
2018-03-20 23:25:55 +0530

    Update MainFile.java

--- MainFile.java
+++ MainFile.java
@@ -94,7 +94,7 @@ public class Main {
         String store = "";
         String OS = System.getProperty("os.name").toLowerCase();
         if(OS.indexOf("win") >= 0){
-            store = "C:/Users/NAUSHAD/";
+            store = "C:/";
         } else if(OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 ){
             store = "/home/";
         } else  if(OS.indexOf("mac") >= 0){
